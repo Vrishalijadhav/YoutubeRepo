5a32d1082df584b59f9ec0b00b369a727a309469
Vrishalijadhav <134845883+Vrishalijadhav@users.noreply.github.com>
2023-05-28 20:21:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,61 +4,62 @@
 # In[1]:
 
 
+pip install streamlit google-api-python-client pymongo
+
+
+# In[5]:
+
+
 import streamlit as st
 
-def get_channel_details(channel_id):
-    # Perform API call to fetch channel details using the channel_id
-    
-    # Dummy data for demonstration purposes
-    channel_details = {
-        "channel_id": channel_id,
-        "title": "Example Channel",
-        "description": "This is an example channel"
-    }
-    
-    return channel_details
 
-# Streamlit app
+# In[6]:
+
+
+from googleapiclient.discovery import build
+
+
+# In[7]:
+
+
+from pymongo import MongoClient
+
+
+# In[8]:
+
+
+def get_channel_details(api_key, channel_id):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    response = youtube.channels().list(part='snippet,statistics', id=channel_id).execute()
+    channel = response['items'][0]
+    return channel
+
+
+# In[11]:
+
+
 def main():
     st.title("YouTube Channel Migration")
 
-    # Channel ID input
-    channel_id = st.text_input("Enter YouTube Channel ID:")
-    
-    # Get channel details
-    if st.button("Get Channel Details"):
-        if not channel_id:
-            st.error("Please enter a valid Channel ID")
-        else:
-            channel_details = get_channel_details(channel_id)
-            st.subheader("Channel Details:")
-            st.write(f"Channel ID: {channel_details['channel_id']}")
-            st.write(f"Title: {channel_details['title']}")
-            st.write(f"Description: {channel_details['description']}")
+    # API key input
+    api_key = st.text_input("Enter your YouTube API keyL:")
 
-    # Select channels for migration
-    st.subheader("Select Channels to Migrate:")
-    channel1 = st.checkbox("Channel 1")
-    channel2 = st.checkbox("Channel 2")
-    channel3 = st.checkbox("Channel 3")
-    # Add more channels as needed
-    
-    # Migrate selected channels
-    if st.button("Migrate Selected Channels"):
-        selected_channels = []
-        if channel1:
-            selected_channels.append("Channel 1")
-        if channel2:
-            selected_channels.append("Channel 2")
-        if channel3:
-            selected_channels.append("Channel 3")
-        # Perform data warehouse migration for selected channels
-        
-        # Display success message
-        if selected_channels:
-            st.success(f"Channels {', '.join(selected_channels)} migrated successfully!")
-        else:
-            st.warning("No channels selected for migration.")
+    # Channel ID input
+    channel_id = st.text_input("Enter the YouTube channel ID:")
+
+    if st.button("Retrieve Channel Details"):
+        channel = get_channel_details(api_key, channel_id)
+        st.write("Channel Details:")
+        st.write(f"Title: {channel['snippet']['title']}")
+        st.write(f"Description: {channel['snippet']['description']}")
+        st.write(f"Subscribers: {channel['statistics']['subscriberCount']}")
+
+        # Connect to MongoDB
+        collection = connect_to_mongodb("mongodb://localhost:27017/", "youtube_data_lake", "channels")
+
+        if st.button("Migrate Channel"):
+            collection.insert_one(channel)
+            st.write("Channel migrated successfully!")
 
 if __name__ == "__main__":
     main()

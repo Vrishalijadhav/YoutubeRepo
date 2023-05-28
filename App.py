0cb4b12d2b86f2878e55529d5514944fc10ce8e4
Vrishalijadhav <134845883+Vrishalijadhav@users.noreply.github.com>
2023-05-28 19:11:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,112 +1,71 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "b3581ca1",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "2023-05-28 18:34:28.785 \n",
-      "  \u001b[33m\u001b[1mWarning:\u001b[0m to view this Streamlit app on a browser, run it with the following\n",
-      "  command:\n",
-      "\n",
-      "    streamlit run C:\\Users\\VRISHALI\\.conda\\envs\\myenv\\lib\\site-packages\\ipykernel_launcher.py [ARGUMENTS]\n"
-     ]
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+def get_channel_details(channel_id):
+    # Perform API call to fetch channel details using the channel_id
+    
+    # Dummy data for demonstration purposes
+    channel_details = {
+        "channel_id": channel_id,
+        "title": "Example Channel",
+        "description": "This is an example channel"
     }
-   ],
-   "source": [
-    "import streamlit as st\n",
-    "\n",
-    "def get_channel_details(channel_id):\n",
-    "    # Perform API call to fetch channel details using the channel_id\n",
-    "    \n",
-    "    # Dummy data for demonstration purposes\n",
-    "    channel_details = {\n",
-    "        \"channel_id\": channel_id,\n",
-    "        \"title\": \"Example Channel\",\n",
-    "        \"description\": \"This is an example channel\"\n",
-    "    }\n",
-    "    \n",
-    "    return channel_details\n",
-    "\n",
-    "# Streamlit app\n",
-    "def main():\n",
-    "    st.title(\"YouTube Channel Migration\")\n",
-    "\n",
-    "    # Channel ID input\n",
-    "    channel_id = st.text_input(\"Enter YouTube Channel ID:\")\n",
-    "    \n",
-    "    # Get channel details\n",
-    "    if st.button(\"Get Channel Details\"):\n",
-    "        if not channel_id:\n",
-    "            st.error(\"Please enter a valid Channel ID\")\n",
-    "        else:\n",
-    "            channel_details = get_channel_details(channel_id)\n",
-    "            st.subheader(\"Channel Details:\")\n",
-    "            st.write(f\"Channel ID: {channel_details['channel_id']}\")\n",
-    "            st.write(f\"Title: {channel_details['title']}\")\n",
-    "            st.write(f\"Description: {channel_details['description']}\")\n",
-    "\n",
-    "    # Select channels for migration\n",
-    "    st.subheader(\"Select Channels to Migrate:\")\n",
-    "    channel1 = st.checkbox(\"Channel 1\")\n",
-    "    channel2 = st.checkbox(\"Channel 2\")\n",
-    "    channel3 = st.checkbox(\"Channel 3\")\n",
-    "    # Add more channels as needed\n",
-    "    \n",
-    "    # Migrate selected channels\n",
-    "    if st.button(\"Migrate Selected Channels\"):\n",
-    "        selected_channels = []\n",
-    "        if channel1:\n",
-    "            selected_channels.append(\"Channel 1\")\n",
-    "        if channel2:\n",
-    "            selected_channels.append(\"Channel 2\")\n",
-    "        if channel3:\n",
-    "            selected_channels.append(\"Channel 3\")\n",
-    "        # Perform data warehouse migration for selected channels\n",
-    "        \n",
-    "        # Display success message\n",
-    "        if selected_channels:\n",
-    "            st.success(f\"Channels {', '.join(selected_channels)} migrated successfully!\")\n",
-    "        else:\n",
-    "            st.warning(\"No channels selected for migration.\")\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    main()\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "78bdb973",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+    
+    return channel_details
+
+# Streamlit app
+def main():
+    st.title("YouTube Channel Migration")
+
+    # Channel ID input
+    channel_id = st.text_input("Enter YouTube Channel ID:")
+    
+    # Get channel details
+    if st.button("Get Channel Details"):
+        if not channel_id:
+            st.error("Please enter a valid Channel ID")
+        else:
+            channel_details = get_channel_details(channel_id)
+            st.subheader("Channel Details:")
+            st.write(f"Channel ID: {channel_details['channel_id']}")
+            st.write(f"Title: {channel_details['title']}")
+            st.write(f"Description: {channel_details['description']}")
+
+    # Select channels for migration
+    st.subheader("Select Channels to Migrate:")
+    channel1 = st.checkbox("Channel 1")
+    channel2 = st.checkbox("Channel 2")
+    channel3 = st.checkbox("Channel 3")
+    # Add more channels as needed
+    
+    # Migrate selected channels
+    if st.button("Migrate Selected Channels"):
+        selected_channels = []
+        if channel1:
+            selected_channels.append("Channel 1")
+        if channel2:
+            selected_channels.append("Channel 2")
+        if channel3:
+            selected_channels.append("Channel 3")
+        # Perform data warehouse migration for selected channels
+        
+        # Display success message
+        if selected_channels:
+            st.success(f"Channels {', '.join(selected_channels)} migrated successfully!")
+        else:
+            st.warning("No channels selected for migration.")
+
+if __name__ == "__main__":
+    main()
+
+
+# In[ ]:
+
+
+
+
